7c500c6f187baa318adaf5689308fad6a5f43bb3
pedja <pedja@bt.foo.org>
2012-09-18 13:00:42 +0200

    Save current settings as profile

--- DualCore/src/rs/pedjaapps/KernelTuner/profiles.java
+++ DualCore/src/rs/pedjaapps/KernelTuner/profiles.java
@@ -4,14 +4,18 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
@@ -25,16 +29,20 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.text.InputType;
 import android.text.method.*;
 import java.util.*;
 
 import rs.pedjaapps.KernelTuner.R;
+
 import android.widget.*;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -47,6 +55,21 @@ public class profiles extends Activity{
 	Spinner spinner;
 	private ProgressDialog pd = null;
 	 private Object data = null;
+	 public String thrupload;
+	 public String thrdownload;
+	 public String cpu0min;
+	 public String cpu1min;
+	 public String cpu0max;
+	 public String cpu1max;
+	 public String curentgovernorcpu0;
+	 public String curentgovernorcpu1;
+	 public String gpu3d;
+	 public String gpu2d;
+	 public String vsync;
+	 public String cpu1;
+	 public boolean cpu1check;
+	 public String name;
+	 
 
 	private class apply extends AsyncTask<String, Void, Object> {
 		String aBuffer = "";
@@ -141,10 +164,390 @@ public class profiles extends Activity{
         }
     	}
 	
+	private class SaveCurrent extends AsyncTask<String, Void, Object> {
+		String aBuffer = "";
+        protected Object doInBackground(String... args) {
+            Log.i("MyApp", "Background thread starting");
+            
+            File file = new File("/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
+			try{
+
+				InputStream fIn = new FileInputStream(file);
+				cpu1check=true;
+
+			}
+
+			catch(FileNotFoundException e){
+				//enable cpu1
+
+
+				Process localProcess;
+				try {
+					localProcess = Runtime.getRuntime().exec("su");
+
+					DataOutputStream localDataOutputStream = new DataOutputStream(localProcess.getOutputStream());
+					localDataOutputStream.writeBytes("echo 0 > /sys/kernel/msm_mpdecision/conf/enabled\n");
+					localDataOutputStream.writeBytes("chmod 666 /sys/devices/system/cpu/cpu1/online\n");
+					localDataOutputStream.writeBytes("echo 1 > /sys/devices/system/cpu/cpu1/online\n");
+					localDataOutputStream.writeBytes("chmod 444 /sys/devices/system/cpu/cpu1/online\n");
+					localDataOutputStream.writeBytes("chown system /sys/devices/system/cpu/cpu1/online\n");
+
+					localDataOutputStream.writeBytes("exit\n");
+					localDataOutputStream.flush();
+					localDataOutputStream.close();
+					localProcess.waitFor();
+					localProcess.destroy();
+					cpu1check=false;
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		
+			}
+            
+            try {
+	 			
+	 			File myFile = new File("/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq");
+	 			FileInputStream fIn = new FileInputStream(myFile);	
+	 			BufferedReader myReader = new BufferedReader(
+	 					new InputStreamReader(fIn));
+	 			String aDataRow = "";
+	 			String aBuffer = "";
+	 			while ((aDataRow = myReader.readLine()) != null) {
+	 				aBuffer += aDataRow + "\n";
+	 			}
+	 			
+	 			cpu0min = aBuffer.trim();
+	 			myReader.close();
+	 			
+	 		} catch (Exception e) {
+	 			cpu0min="err";
+	 		}
+	         
+	         try {
+	  			
+	  			File myFile = new File("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq");
+	  			FileInputStream fIn = new FileInputStream(myFile);	
+	  			BufferedReader myReader = new BufferedReader(
+	  					new InputStreamReader(fIn));
+	  			String aDataRow = "";
+	  			String aBuffer = "";
+	  			while ((aDataRow = myReader.readLine()) != null) {
+	  				aBuffer += aDataRow + "\n";
+	  			}
+	  			
+	  			cpu0max = aBuffer.trim();
+	  			myReader.close();
+	  			
+	  		} catch (Exception e) {
+	  			cpu0max="err";
+	  		}
+	         
+	         try {
+	  			
+	  			File myFile = new File("/sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq");
+	  			FileInputStream fIn = new FileInputStream(myFile);
+			
+	  			BufferedReader myReader = new BufferedReader(
+	  					new InputStreamReader(fIn));
+	  			String aDataRow = "";
+	  			String aBuffer = "";
+	  			while ((aDataRow = myReader.readLine()) != null) {
+	  				aBuffer += aDataRow + "\n";
+	  			}
+	  			
+	  			cpu1min = aBuffer.trim();
+	  			myReader.close();
+	  			
+	  		} catch (Exception e) {
+	  			cpu1min ="err";
+	  		}
+	         
+	         try {
+	  			
+	  			File myFile = new File("/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq");
+	  			FileInputStream fIn = new FileInputStream(myFile);		
+	  			BufferedReader myReader = new BufferedReader(
+	  					new InputStreamReader(fIn));
+	  			String aDataRow = "";
+	  			String aBuffer = "";
+	  			while ((aDataRow = myReader.readLine()) != null) {
+	  				aBuffer += aDataRow + "\n";
+	  			}
+	  			
+	  			cpu1max = aBuffer.trim();
+	  			myReader.close();
+	  			
+	  		} catch (Exception e) {
+	  			cpu1max = "err";
+	  		}
+	         
+	         try {
+	   			
+	   			File myFile = new File("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
+	   			FileInputStream fIn = new FileInputStream(myFile);
+
+	   			BufferedReader myReader = new BufferedReader(
+	   					new InputStreamReader(fIn));
+	   			String aDataRow = "";
+	   			String aBuffer = "";
+	   			while ((aDataRow = myReader.readLine()) != null) {
+	   				aBuffer += aDataRow + "\n";
+	   			}
+	   			
+	   			curentgovernorcpu0 = aBuffer.trim();
+	   			myReader.close();
+	   			
+	   		} catch (Exception e) {
+	   			curentgovernorcpu0="err";
+	   		}
+	         
+	         try {
+	    			
+	    			File myFile = new File("/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor");
+	    			FileInputStream fIn = new FileInputStream(myFile);
+		
+	    			BufferedReader myReader = new BufferedReader(
+	    					new InputStreamReader(fIn));
+	    			String aDataRow = "";
+	    			String aBuffer = "";
+	    			while ((aDataRow = myReader.readLine()) != null) {
+	    				aBuffer += aDataRow + "\n";
+	    			}
+	    			
+	    			curentgovernorcpu1 = aBuffer.trim();
+	    			myReader.close();
+	    			
+	    		} catch (Exception e) {
+				curentgovernorcpu1 = "err";
+	    		}
+	         try {
+	   			
+	   			File myFile = new File("/sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/gpuclk");
+	   			FileInputStream fIn = new FileInputStream(myFile);
+
+	   			BufferedReader myReader = new BufferedReader(
+	   					new InputStreamReader(fIn));
+	   			String aDataRow = "";
+	   			String aBuffer = "";
+	   			while ((aDataRow = myReader.readLine()) != null) {
+	   				aBuffer += aDataRow + "\n";
+	   			}
+
+	   			gpu3d = aBuffer.trim();
+	   			myReader.close();
+	   			
+
+	   			
+	   		} catch (Exception e) {
+	   			gpu3d = "err";
+	   			
+	   		}
+	          
+	          try {
+	    			
+	    			File myFile = new File("/sys/devices/platform/kgsl-2d0.0/kgsl/kgsl-2d0/gpuclk");
+	    			FileInputStream fIn = new FileInputStream(myFile);
+	    			BufferedReader myReader = new BufferedReader(
+	    					new InputStreamReader(fIn));
+	    			String aDataRow = "";
+	    			String aBuffer = "";
+	    			while ((aDataRow = myReader.readLine()) != null) {
+	    				aBuffer += aDataRow + "\n";
+	    			}
+
+	    			gpu2d = aBuffer.trim();
+	    			
+	    			myReader.close();
+	  	
+	    		} catch (Exception e) {
+	 			gpu2d = "err";
+	    		}
+	          try {
+	        		String aBuffer = "";
+	        	 			File myFile = new File("/sys/kernel/debug/msm_fb/0/vsync_enable");
+	        	 			FileInputStream fIn = new FileInputStream(myFile);
+	        	 			BufferedReader myReader = new BufferedReader(
+	        	 					new InputStreamReader(fIn));
+	        	 			String aDataRow = "";
+	        	 			while ((aDataRow = myReader.readLine()) != null) {
+	        	 				aBuffer += aDataRow + "\n";
+	        	 			}
+
+	        	 			vsync = aBuffer.trim();
+	        	 			myReader.close();
+	        	 	
+	        	 		} catch (Exception e) {
+							vsync ="err";
+	        	 		}
+	          try {
+
+	    		   File myFile = new File("/sys/kernel/msm_mpdecision/conf/nwns_threshold_up");
+	    		   FileInputStream fIn = new FileInputStream(myFile);
+
+	    		   BufferedReader myReader = new BufferedReader(
+	    			   new InputStreamReader(fIn));
+	    		   String aDataRow = "";
+	    		   String aBuffer = "";
+	    		   while ((aDataRow = myReader.readLine()) != null) {
+	    			   aBuffer += aDataRow + "\n";
+	    		   }
+
+	    		   thrupload = aBuffer;
+	    		   myReader.close();
+
+	    	   } catch (Exception e) {
+	    		   thrupload="err";
+	    		  
+	    	   }
+	          
+	          try {
+
+	    		   File myFile = new File("/sys/kernel/msm_mpdecision/conf/nwns_threshold_down");
+	    		   FileInputStream fIn = new FileInputStream(myFile);
+
+	    		   BufferedReader myReader = new BufferedReader(
+	    			   new InputStreamReader(fIn));
+	    		   String aDataRow = "";
+	    		   String aBuffer = "";
+	    		   while ((aDataRow = myReader.readLine()) != null) {
+	    			   aBuffer += aDataRow + "\n";
+	    		   }
+
+	    		   thrdownload = aBuffer;
+	    		   myReader.close();
+
+	    	   } catch (Exception e) {
+	    		   thrdownload="err";
+	    		   
+	    	   }
+	          
+	          if(cpu1check==true){
+	        	  cpu1="2";
+	        	  
+	          }
+	          else if(cpu1check==false){
+	        	  cpu1="1";
+	          }
+	          
+	          if(cpu1check==false){
+	  			Process localProcess;
+					try {
+						localProcess = Runtime.getRuntime().exec("su");
+
+						DataOutputStream localDataOutputStream = new DataOutputStream(localProcess.getOutputStream());
+						localDataOutputStream.writeBytes("echo 1 > /sys/kernel/msm_mpdecision/conf/enabled\n");
+						localDataOutputStream.writeBytes("chmod 777 /sys/devices/system/cpu/cpu1/online\n");
+						localDataOutputStream.writeBytes("echo 0 > /sys/devices/system/cpu/cpu1/online\n");
+						localDataOutputStream.writeBytes("chown system /sys/devices/system/cpu/cpu1/online\n");
+						localDataOutputStream.writeBytes("exit\n");
+						localDataOutputStream.flush();
+						localDataOutputStream.close();
+						localProcess.waitFor();
+						localProcess.destroy();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+	          
+            
+			
+            return aBuffer;
+        }
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+          //  DualCore.this.pd = ProgressDialog.show(DualCore.this, "Working..", "Checking for updates...", true, false );
+    	//	pd.setCancelable(true);
+    	
+    		pd = ProgressDialog.show(
+    			profiles.this,
+    			"Working...",
+    			"Checking for updates...",
+    			true,
+    			true
+    			
+    				
+    			
+    			
+            );
+        }
+        
+        protected void onPostExecute(Object result) {
+        	ArrayAdapter myAdap = (ArrayAdapter) spinner.getAdapter(); //cast to an ArrayAdapter
+			  myAdap.clear();
+			  db.addProfile(new Profile(name,cpu0min, 
+	    	    		cpu0max, 
+	    	    		cpu1min,
+	    	    		cpu1max,
+	    	    		curentgovernorcpu0,
+	    	    		curentgovernorcpu1,
+	    	    		gpu2d,
+	    	    		gpu3d,
+	    	    		vsync,
+	    	    		cpu1,
+	    	    		thrupload,
+	    	    		thrdownload));
+	  
+	  getprofiles();
+	  //spinnerProfiles(); 
+	 
+		int spinnerPosition = myAdap.getPosition(name);
+
+		//set the default according to value
+		spinner.setSelection(spinnerPosition);
+        	pd.dismiss();
+            profiles.this.data = result;
+
+        }
+    	}
+	
     	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.profiles);
+	
+	Button current = (Button)findViewById(R.id.button1);
+	current.setOnClickListener(new OnClickListener(){
+
+		public void onClick(View arg0) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(arg0.getContext());
+
+			builder.setTitle("Profile Name:");
+
+			builder.setMessage("Enter Profile Name: ");
+
+			builder.setIcon(R.drawable.ic_menu_cc);
+
+			 final EditText input = new EditText(arg0.getContext());//)layout.findViewById(R.id.editText2);
+			
+           
+			
+			
+			input.setGravity(Gravity.CENTER_HORIZONTAL);
+			//input.selectAll();
+			builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					name = String.valueOf(input.getText());
+					new SaveCurrent().execute();
+				 
+				}
+			});
+           builder.setView(input);
+			
+			AlertDialog alert = builder.create();
+
+			alert.show();
+		}
+		
+	});
 	
 	db = new DatabaseHandler(this);
 	 
