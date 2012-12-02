dc66f863d35c0717c5427cedb96363457551a482
pedja <pedja@bt.foo.org>
2012-12-02 11:26:48 +0100

    [Redesigned About Screen]

--- src/rs/pedjaapps/KernelTuner/About.java
+++ src/rs/pedjaapps/KernelTuner/About.java
@@ -1,11 +1,16 @@
 package rs.pedjaapps.KernelTuner;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class About extends Activity
@@ -20,21 +25,95 @@ public class About extends Activity
 		setContentView(R.layout.about);
 
 		
-		TextView	official = (TextView) findViewById(R.id.textView8);
-		TextView	xda = (TextView) findViewById(R.id.textView6);
-		TextView versiontext = (TextView)findViewById(R.id.textView3);
 		
+		TextView versiontext = (TextView)findViewById(R.id.textView1);
 		
-		if (xda != null)
+		TextView mail = (TextView)findViewById(R.id.textView6);
+		ImageView gp = (ImageView)findViewById(R.id.imageView1);
+		ImageView fb = (ImageView)findViewById(R.id.imageView2);
+		ImageView tw = (ImageView)findViewById(R.id.imageView3);
+		ImageView gpl = (ImageView)findViewById(R.id.imageView4);
+		ImageView xda = (ImageView)findViewById(R.id.imageView5);
+		ImageView anthrax = (ImageView)findViewById(R.id.imageView6);
+		ImageView kt = (ImageView)findViewById(R.id.imageView7);
+		
+		gp.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.setData(Uri.parse("market://details?id=rs.pedjaapps.KernelTuner"));
+		        startActivity(intent);
+		    }
+		});
+		
+		fb.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://www.facebook.com/KernelTuner"));
+		        startActivity(intent);
+		    }
+		});
+		
+		tw.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://twitter.com/kerneltuner"));
+		        startActivity(intent);
+		    }
+		});
+		
+		gpl.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("https://plus.google.com/u/0/b/115273553144904124119/115273553144904124119"));
+		        startActivity(intent);
+		    }
+		});
+		
+		kt.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://kerneltuner.pedjaapps.in.rs"));
+		        startActivity(intent);
+		    }
+		});
+		
+		xda.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://forum.xda-developers.com/showthread.php?t=1719934"));
+		        startActivity(intent);
+		    }
+		});
+		
+		anthrax.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://anthrax-kernels.us/forum/showthread.php?t=574"));
+		        startActivity(intent);
+		    }
+		});
+		
+		if (mail != null)
 		{
-			xda.setMovementMethod(LinkMovementMethod.getInstance());
+			mail.setMovementMethod(LinkMovementMethod.getInstance());
 		}
 
+	
+
 		
-		if (official != null)
-		{
-			official.setMovementMethod(LinkMovementMethod.getInstance());
-		}
 
 		
 		try
