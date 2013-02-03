51666e6d3425addc179eaecfa5bacb85df687f73
pedja <pedja@bt.foo.org>
2013-02-03 14:23:35 +0100

    Improved backup and restore

--- src/rs/pedjaapps/KernelTuner/BackupRestore.java
+++ src/rs/pedjaapps/KernelTuner/BackupRestore.java
@@ -6,16 +6,31 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -24,6 +39,106 @@ import android.widget.Toast;
 
 public class BackupRestore extends SherlockActivity {
 
+	
+	private String prefBackupMessage;
+	private String dbBackupMessage;
+	private String prefRestoreMessage;
+	private String dbRestoreMessage;
+	private class Backup extends AsyncTask<String, Integer, String> {
+
+		ProgressDialog pd;
+		@Override
+		protected String doInBackground(String... args) {
+			
+			publishProgress(0);
+			backupUserPrefs(BackupRestore.this);
+			publishProgress(1);
+			backupDb(BackupRestore.this);
+				
+			
+			return "";
+		}
+
+		@Override
+		protected void onPreExecute(){
+			pd = new ProgressDialog(BackupRestore.this);
+			pd.setIndeterminate(true);
+			pd.setTitle("Backing Up Application Settings");
+			pd.show();
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... values)
+		{
+			switch(values[0]){
+			case 0:
+				pd.setMessage("Backing up Database");
+				pd.setProgress(1);
+				break;
+			case 1:
+				pd.setMessage("Backing up Shared Preferences");
+				pd.setProgress(0);
+				break;
+			}
+			super.onProgressUpdate();
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			pd.cancel();
+			alert("Backup", prefBackupMessage, dbBackupMessage, "");
+		}
+
+	}
+
+	private class Restore extends AsyncTask<String, Integer, String> {
+
+		ProgressDialog pd;
+		@Override
+		protected String doInBackground(String... args) {
+			
+			publishProgress(0);
+			restoreUserPrefs(BackupRestore.this);
+			publishProgress(1);
+			restoreDb(BackupRestore.this);
+				
+			
+			return "";
+		}
+
+		@Override
+		protected void onPreExecute(){
+			pd = new ProgressDialog(BackupRestore.this);
+			pd.setIndeterminate(true);
+			pd.setTitle("Restoring Application Settings");
+			pd.show();
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... values)
+		{
+			switch(values[0]){
+			case 0:
+				pd.setMessage("Restoring Database");
+				pd.setProgress(1);
+				break;
+			case 1:
+				pd.setMessage("Restoring Shared Preferences");
+				pd.setProgress(0);
+				break;
+			}
+			super.onProgressUpdate();
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			pd.cancel();
+			alert("Restore", prefRestoreMessage, dbRestoreMessage, "Restart Application for changes to take effect");
+			System.out.println(""+prefRestoreMessage);
+		}
+
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,7 +165,15 @@ public class BackupRestore extends SherlockActivity {
 
 			@Override
 			public void onClick(View arg0) {
-				backup();
+				//backup();
+				File sd = Environment.getExternalStorageDirectory();
+				if(sd.canWrite()){
+					new Backup().execute();
+				}
+				else{
+					Toast.makeText(BackupRestore.this, "Unable to write to external storage",
+							Toast.LENGTH_LONG).show();
+				}
 			}
 
 		});
@@ -59,114 +182,81 @@ public class BackupRestore extends SherlockActivity {
 
 			@Override
 			public void onClick(View arg0) {
-				restore();
+				File sd = Environment.getExternalStorageDirectory();
+				if(sd.canRead()){
+					new Restore().execute();
+				}
+				else{
+					Toast.makeText(BackupRestore.this, "Unable to read from external storage",
+							Toast.LENGTH_LONG).show();
+				}
 			}
 
 		});
 	}
 
-	private void backup() {
-		try {
-			File sd = Environment.getExternalStorageDirectory();
-			File data = Environment.getDataDirectory();
+	
 
-			if (sd.canWrite()) {
-				String[] currentPaths = {
-						"//data//rs.pedjaapps.KernelTuner//databases//KernelTuner.db",
-						"//data//rs.pedjaapps.KernelTuner//shared_prefs" };
-				String[] backupPaths = { "KernelTuner/KernelTuner.db",
-					"KernelTuner/shared_prefs" };
+	
+	public void backupUserPrefs(Context context) {
+		  final File prefsFile = new File(context.getFilesDir(), "../shared_prefs/" + 
+		      context.getPackageName()+"_preferences.xml");
+		  final File backupFile = new File(Environment.getExternalStorageDirectory(), 
+		      "KernelTuner/preferenceBackup.xml");
+		  
+		  
+		  try {
+			  FileInputStream fin = new FileInputStream(prefsFile);
+			  FileOutputStream fout = new FileOutputStream(backupFile);
+		    FileChannel src = fin.getChannel();
+		    FileChannel dst = fout.getChannel();
+		    
+		    dst.transferFrom(src, 0, src.size());
 
-				for (int i = 0; i < currentPaths.length; i++) {
-					File currentDB = new File(data, currentPaths[i]);
-					File backupDB = new File(sd, backupPaths[i]);
-
-					copyDirectory(currentDB, backupDB);
-					Toast.makeText(this, "Backup Successful", Toast.LENGTH_LONG)
-							.show();
-
-				}
-
-			} else {
-				Toast.makeText(this, "External Storage not Mounted",
-						Toast.LENGTH_LONG).show();
-			}
-		} catch (Exception e) {
-			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+		    src.close();
+		    dst.close();
+		    fin.close();
+		    fout.close();
+		    prefBackupMessage = "Backup Successful";
+		    //return true;
+		  } catch (FileNotFoundException e) {
+		    prefBackupMessage = e.getMessage();
+		    //return false;
+		  } catch (IOException e) {
+			  prefBackupMessage = e.getMessage();
+		    //return false;
+		  }
 		}
-	}
 
-	private void restore() {
-		try {
-			File sd = Environment.getExternalStorageDirectory();
-			File data = Environment.getDataDirectory();
+	public void backupDb(Context context) {
+		  final File prefsFile = new File(context.getFilesDir(), "../databases/KTDatabase.db");
+		  final File backupFile = new File(Environment.getExternalStorageDirectory(), 
+		      "KernelTuner/KTDatabase.db");
+		  
+		  
+		  try {
+			  FileInputStream fin = new FileInputStream(prefsFile);
+			  FileOutputStream fout = new FileOutputStream(backupFile);
+		    FileChannel src = fin.getChannel();
+		    FileChannel dst = fout.getChannel();
+		    
+		    dst.transferFrom(src, 0, src.size());
 
-			if (sd.canWrite()) {
-				String[] currentPaths = {
-					"//data//rs.pedjaapps.KernelTuner//databases//KernelTuner.db",
-					"//data//rs.pedjaapps.KernelTuner//shared_prefs" };
-				String[] backupPaths = { "KernelTuner/KernelTuner.db",
-					"KernelTuner/shared_prefs" };
-
-				for (int i = 0; i < currentPaths.length; i++) {
-					File currentDB = new File(data, currentPaths[i]);
-					File backupDB = new File(sd, backupPaths[i]);
-
-					copyDirectory(backupDB, currentDB);
-					Toast.makeText(this, "Restore Successful",
-							Toast.LENGTH_LONG).show();
-
-				}
-
-			} else {
-				Toast.makeText(this, "External Stirage not Mounted",
-						Toast.LENGTH_LONG).show();
-			}
-		} catch (Exception e) {
-			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+		    src.close();
+		    dst.close();
+		    fin.close();
+		    fout.close();
+		    dbBackupMessage = "Backup Successful";
+		    //return true;
+		  } catch (FileNotFoundException e) {
+		    dbBackupMessage = e.getMessage();
+		    //return false;
+		  } catch (IOException e) {
+			  dbBackupMessage = e.getMessage();
+		    //return false;
+		  }
 		}
-	}
-
-	public void copyDirectory(File sourceLocation, File targetLocation) {
-
-		if (sourceLocation.isDirectory()) {
-			if (!targetLocation.exists() && !targetLocation.mkdirs()) {
-			}
-
-			String[] children = sourceLocation.list();
-			for (int i = 0; i < children.length; i++) {
-				copyDirectory(new File(sourceLocation, children[i]), new File(
-						targetLocation, children[i]));
-			}
-		} else {
-
-			// make sure the directory we plan to store the recording in exists
-			File directory = targetLocation.getParentFile();
-			if (directory != null && !directory.exists() && !directory.mkdirs()) {
-			}
-
-			try {
-				InputStream in = new FileInputStream(sourceLocation);
-				OutputStream out = new FileOutputStream(targetLocation);
-
-				// Copy the bits from instream to outstream
-				byte[] buf = new byte[1024];
-				int len;
-				while ((len = in.read(buf)) > 0) {
-					out.write(buf, 0, len);
-				}
-				in.close();
-				out.close();
-			} catch (FileNotFoundException e) {
-				Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-
-			} catch (IOException e) {
-				Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-			}
-
-		}
-	}
-
+	
 	@Override
 	public boolean onOptionsItemSelected(com.actionbarsherlock.view.MenuItem item) {
 	    switch (item.getItemId()) {
@@ -180,6 +270,128 @@ public class BackupRestore extends SherlockActivity {
 	            
 	    }
 	    return super.onOptionsItemSelected(item);
+	}
+	
+	public void restoreDb(Context context) {
+		  final File prefsFile = new File(context.getFilesDir(), "../databases/KTDatabase.db");
+		  final File backupFile = new File(Environment.getExternalStorageDirectory(), 
+		      "KernelTuner/KTDatabase.db");
+		  
+		  
+		  try {
+			  FileInputStream fin = new FileInputStream(backupFile);
+			  FileOutputStream fout = new FileOutputStream(prefsFile);
+		    FileChannel src = fin.getChannel();
+		    FileChannel dst = fout.getChannel();
+		    
+		    dst.transferFrom(src, 0, src.size());
+
+		    src.close();
+		    dst.close();
+		    fin.close();
+		    fout.close();
+		    dbRestoreMessage = "Restore Successful";
+		    //return true;
+		  } catch (FileNotFoundException e) {
+		    dbRestoreMessage = e.getMessage();
+		    //return false;
+		  } catch (IOException e) {
+			  dbRestoreMessage = e.getMessage();
+		    //return false;
+		  }
+		}
+	
+	
+	public void restoreUserPrefs(Context context) {
+		  final File backupFile = new File(Environment.getExternalStorageDirectory(), 
+		      "KernelTuner/preferenceBackup.xml");
+		  
+		  try {
+		      
+		    SharedPreferences sharedPreferences = PreferenceManager
+		        .getDefaultSharedPreferences(context);
+		    
+		    Editor editor = sharedPreferences.edit();
+		      
+		    InputStream inputStream = new FileInputStream(backupFile);
+
+		    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+		    Document doc = docBuilder.parse(inputStream);        
+		    Element root = doc.getDocumentElement();
+		        
+		    Node child = root.getFirstChild();
+		    while(child!=null) {  
+		      if(child.getNodeType() == Node.ELEMENT_NODE) {
+		          
+		        Element element = (Element)child;
+		            
+		        String type = element.getNodeName();
+		        String name = element.getAttribute("name");
+		            
+		        // In my app, all prefs seem to get serialized as either "string" or
+		        // "boolean" - this will need expanding if yours uses any other types!    
+		        if(type.equals("string")) {
+		          String value = element.getTextContent();
+		          editor.putString(name, value);
+		        }
+		        else if(type.equals("boolean")) {
+		          String value = element.getAttribute("value");
+		          editor.putBoolean(name, value.equals("true"));
+		        }
+		      }
+		            
+		      child = child.getNextSibling();
+		          
+		    }
+		        
+		    editor.commit();
+
+		   prefRestoreMessage = "Restore Successful"; 
+		    
+		    
+		  } catch (FileNotFoundException e) {
+			  prefRestoreMessage = e.getMessage(); 
+		    
+		  } catch (ParserConfigurationException e) {
+			  prefRestoreMessage = e.getMessage();
+		    
+		  } catch (SAXException e) {
+			  prefRestoreMessage = e.getMessage();
+		  } catch (IOException e) {
+			  prefRestoreMessage = e.getMessage();
+		  }
+		  
+		  
+		  
+		}
+
+	private void alert(String title, String prefMessage, String dbMessage, String restoreMessage){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setTitle(title);
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("Shared Preferences - ");
+		strBuilder.append(prefMessage+"\n\n");
+		strBuilder.append("Database - ");
+		strBuilder.append(dbMessage+"\n\n");
+		strBuilder.append(restoreMessage);
+		builder.setMessage(strBuilder.toString());
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface arg0, int arg1) {
+				prefBackupMessage = null;
+				dbBackupMessage = null;
+				prefRestoreMessage = null;
+				dbRestoreMessage = null;
+				
+			}
+		});
+		AlertDialog alert = builder.create();
+
+		alert.show();
 	}
 	
 }
