f2a05e31973a9242020d928245412f19bd93c43e
Predrag Cokulov <predragcokulov@gmail.com>
2014-07-20 15:49:45 +0200

    Minor tweaks in About screen

--- src/rs/pedjaapps/kerneltuner/ui/About.java
+++ src/rs/pedjaapps/kerneltuner/ui/About.java
@@ -19,6 +19,7 @@
 package rs.pedjaapps.kerneltuner.ui;
 
 import rs.pedjaapps.kerneltuner.R;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -31,107 +32,94 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 
-
 public class About extends Activity
 {
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
-	final TextView versiontext = (TextView)findViewById(R.id.textView1);
-		
-		final TextView mail = (TextView)findViewById(R.id.textView6);
-		final ImageView gp = (ImageView)findViewById(R.id.imageView1);
-		final ImageView fb = (ImageView)findViewById(R.id.imageView2);
-		final ImageView tw = (ImageView)findViewById(R.id.imageView3);
-		final ImageView gpl = (ImageView)findViewById(R.id.imageView4);
-		final ImageView xda = (ImageView)findViewById(R.id.imageView5);
-		final ImageView anthrax = (ImageView)findViewById(R.id.imageView6);
-		final ImageView kt = (ImageView)findViewById(R.id.imageView7);
-		
-		gp.setOnClickListener(new View.OnClickListener(){
-		    public void onClick(View v){
-		        Intent intent = new Intent();
-		        intent.setAction(Intent.ACTION_VIEW);
-		        intent.setData(Uri.parse("market://details?id=rs.pedjaapps.KernelTuner"));
-		        startActivity(intent);
-		    }
-		});
-		
-		fb.setOnClickListener(new View.OnClickListener(){
-		    public void onClick(View v){
-		        Intent intent = new Intent();
-		        intent.setAction(Intent.ACTION_VIEW);
-		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-		        intent.setData(Uri.parse("http://www.facebook.com/KernelTuner"));
-		        startActivity(intent);
-		    }
-		});
-		
-		tw.setOnClickListener(new View.OnClickListener(){
-		    public void onClick(View v){
-		        Intent intent = new Intent();
-		        intent.setAction(Intent.ACTION_VIEW);
-		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-		        intent.setData(Uri.parse("http://twitter.com/kerneltuner"));
-		        startActivity(intent);
-		    }
-		});
-		
-		gpl.setOnClickListener(new View.OnClickListener(){
-		    public void onClick(View v){
-		        Intent intent = new Intent();
-		        intent.setAction(Intent.ACTION_VIEW);
-		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-		        intent.setData(Uri.parse("https://plus.google.com/u/0/b/115273553144904124119/115273553144904124119"));
-		        startActivity(intent);
-		    }
-		});
-		
-		kt.setOnClickListener(new View.OnClickListener(){
-		    public void onClick(View v){
-		        Intent intent = new Intent();
-		        intent.setAction(Intent.ACTION_VIEW);
-		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-		        intent.setData(Uri.parse("http://kerneltuner.pedjaapps.in.rs"));
-		        startActivity(intent);
-		    }
-		});
-		
-		xda.setOnClickListener(new View.OnClickListener(){
-		    public void onClick(View v){
-		        Intent intent = new Intent();
-		        intent.setAction(Intent.ACTION_VIEW);
-		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-		        intent.setData(Uri.parse("http://forum.xda-developers.com/showthread.php?t=1719934"));
-		        startActivity(intent);
-		    }
-		});
-		
-		anthrax.setOnClickListener(new View.OnClickListener(){
-		    public void onClick(View v){
-		        Intent intent = new Intent();
-		        intent.setAction(Intent.ACTION_VIEW);
-		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-		        intent.setData(Uri.parse("http://anthrax-kernels.us/forum/showthread.php?t=574"));
-		        startActivity(intent);
-		    }
-		});
-		
-		if (mail != null)
-		{
-			mail.setMovementMethod(LinkMovementMethod.getInstance());
-		}
-		try
-		{
-			PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-			String	version = pInfo.versionName;
-			versiontext.setText(getResources().getString(R.string.version) + version);
-		}
-		catch (PackageManager.NameNotFoundException e)
-		{
-		}
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        final TextView versiontext = (TextView) findViewById(R.id.textView1);
+
+        final TextView mail = (TextView) findViewById(R.id.textView6);
+        final ImageView gp = (ImageView) findViewById(R.id.imageView1);
+        final ImageView gpl = (ImageView) findViewById(R.id.imageView4);
+        final ImageView xda = (ImageView) findViewById(R.id.imageView5);
+        final ImageView github = (ImageView) findViewById(R.id.imageView8);
+        final ImageView kt = (ImageView) findViewById(R.id.imageView7);
+
+        gp.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("market://details?id=rs.pedjaapps.kerneltuner"));
+                startActivity(intent);
+            }
+        });
+
+        gpl.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://plus.google.com/u/0/b/115273553144904124119/115273553144904124119"));
+                startActivity(intent);
+            }
+        });
+
+        kt.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://pedjaapps.net"));
+                startActivity(intent);
+            }
+        });
+
+        xda.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://forum.xda-developers.com/showthread.php?t=1719934"));
+                startActivity(intent);
+            }
+        });
+
+        github.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://github.com/pedja1/Kernel-Tuner"));
+                startActivity(intent);
+            }
+        });
+
+        if (mail != null)
+        {
+            mail.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+        try
+        {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+            versiontext.setText(getResources().getString(R.string.version) + version);
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+        }
+    }
 }
